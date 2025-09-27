e580c129a0f6981e0f8a8a550ad6bd8a5e0c33b8
Alopach-2023171021 <2023171021@uteq.edu.mx>
2025-09-27 02:29:06 -0600

    Estructura inicial del microservicio de divisiones

---
+++ src/test/java/mx/edu/uteq/idgs12/microservicio_division/MicroservicioDivisionApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs12.microservicio_division;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDivisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
