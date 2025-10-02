93d5728f3fee0c6a72b98512835137dc8f3e9659
alberto <alberto.delgadillo@uteq.edu.mx>
2025-10-02 17:30:44 -0600

    feature/idsg12-listar-division se ha concluido

--- src/main/java/mx/edu/uteq/idgs12/microservicio_division/controller/DivisionController.java
+++ src/main/java/mx/edu/uteq/idgs12/microservicio_division/controller/DivisionController.java
@@ -1,10 +1,26 @@
 package mx.edu.uteq.idgs12.microservicio_division.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import mx.edu.uteq.idgs12.microservicio_division.dto.DivisionToViewListDto;
+import mx.edu.uteq.idgs12.microservicio_division.service.DivisionService;
 
 @RestController
 @RequestMapping("/api/division")
 public class DivisionController {
-    
+    @Autowired
+	private  DivisionService divisionService;
+
+
+    @GetMapping
+	public List<DivisionToViewListDto> getAllDivisiones() {
+		return divisionService.findAll();
+	}
+
+
 }
