266f229be7a82fb273d626338f3330ddef1597e0
Ricardo <rica82561@gmail.com>
2025-10-05 17:07:46 -0600

    Ignora este pro

---
+++ src/main/java/mx/edu/uteq/idgs12/microservicio_division/controller/Test.java
@@ -0,0 +1,11 @@
+package mx.edu.uteq.idgs12.microservicio_division.controller;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Este es un archivo de prueba");
+
+    }
+
+}
