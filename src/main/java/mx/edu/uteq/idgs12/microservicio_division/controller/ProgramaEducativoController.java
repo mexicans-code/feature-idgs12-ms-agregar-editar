54f16bc47dd8efdc4267a807c04a3e7caab69fac
ceci222001 <2023171020@uteq.edu.mx>
2025-10-05 01:48:45 -0600

    Feature buscar por todos y por id

--- src/main/java/mx/edu/uteq/idgs12/microservicio_division/controller/ProgramaEducativoController.java
+++ src/main/java/mx/edu/uteq/idgs12/microservicio_division/controller/ProgramaEducativoController.java
@@ -3,13 +3,26 @@ package mx.edu.uteq.idgs12.microservicio_division.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.idgs12.microservicio_division.dto.ProgramaEducativoToViewListDto;
 
 import mx.edu.uteq.idgs12.microservicio_division.service.ProgramaEducativoService;
 
 @RestController
 @RequestMapping("/api")
 public class ProgramaEducativoController {
+
     @Autowired
     private ProgramaEducativoService programaEducativoService;
+
+    @GetMapping("/programas")
+    public List<ProgramaEducativoToViewListDto> findAll() {
+        return programaEducativoService.findAll();
+    }
+
+    @GetMapping("/programas/{id}")
+    public ProgramaEducativoToViewListDto findById(@PathVariable Long id) {
+        return programaEducativoService.findById(id);
+    }
+
 
 }
