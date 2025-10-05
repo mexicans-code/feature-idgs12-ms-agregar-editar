88d32c24862d0722f79567902c24bdca9f4306e3
ceci222001 <2023171020@uteq.edu.mx>
2025-10-05 01:53:09 -0600

    Feature buscar por todos y por id

--- src/main/java/mx/edu/uteq/idgs12/microservicio_division/entity/ProgramaEducativo.java
+++ src/main/java/mx/edu/uteq/idgs12/microservicio_division/entity/ProgramaEducativo.java
@@ -1,20 +1,20 @@
 package mx.edu.uteq.idgs12.microservicio_division.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.GenerationType;
 
 @Data
 @Entity
 @Table(name = "programas_educativos")
 public class ProgramaEducativo {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String programa;
     private boolean activo;
+
+    @ManyToOne
+    @JoinColumn(name = "division_id")
+    private DivisionEntity division;
 }
